8a49242c553affed07f9490bef58ae7b6f463aaf
Maylse <151539643+Maylse@users.noreply.github.com>
2024-10-25 01:16:37 +0800

    changes env

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/test', function () {
-    return response()->json(['message' => 'API is working']);
+    return response()->json(['message' => 'This is a test endpoint.']);
 });
 
 //ENTRY POINTS
